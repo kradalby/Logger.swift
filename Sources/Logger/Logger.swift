e6c441b6a2f51f2ff6ee0350b0e5d0344087a7e9
Kristoffer Dalby <kradalby@kradalby.no>
2018-01-21 16:00:15 +0100

    adds basic loglevel filtering

--- Sources/Logger/Logger.swift
+++ Sources/Logger/Logger.swift
@@ -2,25 +2,53 @@ import Foundation
 
 import Rainbow
 
+public enum LogLevel {
+    case ERROR
+    case WARNING
+    case INFO
+    case DEBUG
+    case TRACE
+}
+
 public struct Logger {
-    public func debug(_ message: Any) {
-        print("[DEBUG] ".lightCyan + "\(message)")
-    }
-    public func warning(_ message: Any) {
-        print("[WARNING] ".yellow + "\(message)")
-    }
-    public func info(_ message: Any) {
-        print("[INFO] ".green + "\(message)")
-    }
-    public func trace(_ message: Any) {
-        print("[TRACE] ".white + "\(message)")
-    }
+    public let logLevel: LogLevel
+    
     public func error(_ message: Any) {
         print("[ERROR] ".red + "\(message)")
     }
-    
+    public func warning(_ message: Any) {
+        if logLevel == .TRACE ||
+            logLevel == .DEBUG ||
+            logLevel == .INFO ||
+            logLevel == .WARNING {
+        print("[WARNING] ".yellow + "\(message)")
+        }
+    }
+    public func info(_ message: Any) {
+        if logLevel == .TRACE ||
+            logLevel == .DEBUG ||
+            logLevel == .INFO {
+        print("[INFO] ".green + "\(message)")
+        }
+    }
+    public func debug(_ message: Any) {
+        if logLevel == .TRACE ||
+            logLevel == .DEBUG {
+            print("[DEBUG] ".lightCyan + "\(message)")
+        }
+    }
+    public func trace(_ message: Any) {
+        if logLevel == .TRACE {
+            print("[TRACE] ".white + "\(message)")
+        }
+    }
+
     public init () {
-        
+        self.logLevel = LogLevel.INFO
+    }
+    
+    public init(_ logLevel: LogLevel) {
+        self.logLevel = logLevel
     }
 }
 
