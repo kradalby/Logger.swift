5ee3872802d4e4d44e0bd5406d78fe0e4bb999a6
Kristoffer Dalby <kradalby@kradalby.no>
2020-07-02 17:55:50 +0100

    Reformat and bump tools

--- Sources/Logger/Logger.swift
+++ Sources/Logger/Logger.swift
@@ -1,68 +1,64 @@
 import Foundation
-
 import Rainbow
 
 public enum LogLevel: Int {
-    case ERROR = 1
-    case WARNING = 2
-    case INFO = 3
-    case DEBUG = 4
-    case TRACE = 5
-    
+  case ERROR = 1
+  case WARNING = 2
+  case INFO = 3
+  case DEBUG = 4
+  case TRACE = 5
 
 }
 
 public struct Logger {
-    public let logLevel: LogLevel
-    
-    public func error(_ message: Any) {
-        print("[ERROR] ".red + "\(message)")
-    }
-    public func warning(_ message: Any) {
-        if logLevel.rawValue >= 2 {
-        print("[WARNING] ".yellow + "\(message)")
-        }
-    }
-    public func info(_ message: Any) {
-        if logLevel.rawValue >= 3 {
-        print("[INFO] ".green + "\(message)")
-        }
-    }
-    public func debug(_ message: Any) {
-        if logLevel.rawValue >= 4 {
-            print("[DEBUG] ".lightCyan + "\(message)")
-        }
-    }
-    public func trace(_ message: Any) {
-        if logLevel.rawValue >= 5 {
-            print("[TRACE] ".white + "\(message)")
-        }
-    }
+  public let logLevel: LogLevel
 
-    public init () {
-        self.logLevel = LogLevel.INFO
+  public func error(_ message: Any) {
+    print("[ERROR] ".red + "\(message)")
+  }
+  public func warning(_ message: Any) {
+    if logLevel.rawValue >= 2 {
+      print("[WARNING] ".yellow + "\(message)")
     }
-    
-    public init(_ logLevel: LogLevel) {
-        self.logLevel = logLevel
+  }
+  public func info(_ message: Any) {
+    if logLevel.rawValue >= 3 {
+      print("[INFO] ".green + "\(message)")
     }
-    
-    public init(_ logLevel: Int) {
-        switch logLevel {
-        case 1:
-            self.logLevel = .ERROR
-        case 2:
-            self.logLevel = .WARNING
-        case 3:
-            self.logLevel = .INFO
-        case 4:
-            self.logLevel = .DEBUG
-        case 5:
-            self.logLevel = .TRACE
-        default:
-            self.logLevel = .INFO
-        }
+  }
+  public func debug(_ message: Any) {
+    if logLevel.rawValue >= 4 {
+      print("[DEBUG] ".lightCyan + "\(message)")
     }
+  }
+  public func trace(_ message: Any) {
+    if logLevel.rawValue >= 5 {
+      print("[TRACE] ".white + "\(message)")
+    }
+  }
+
+  public init() {
+    self.logLevel = LogLevel.INFO
+  }
+
+  public init(_ logLevel: LogLevel) {
+    self.logLevel = logLevel
+  }
+
+  public init(_ logLevel: Int) {
+    switch logLevel {
+    case 1:
+      self.logLevel = .ERROR
+    case 2:
+      self.logLevel = .WARNING
+    case 3:
+      self.logLevel = .INFO
+    case 4:
+      self.logLevel = .DEBUG
+    case 5:
+      self.logLevel = .TRACE
+    default:
+      self.logLevel = .INFO
+    }
+  }
 }
-
-
